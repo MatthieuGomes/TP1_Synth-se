8cdf8dcaf909148fc367056f68b59c0de26bf009
matthieuGomes <matthieu.gomes@ensea.fr>
2024-11-28 23:17:27 +0100

    remove magic number

--- src/functions.c
+++ src/functions.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "functions.h"
+#define MAX_INPUT_SIZE 1024 // Choose a maximum input size pretty large to avoi
 char *welcome_message = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez \'exit\'\n";
 char *prompt_message = "enseash % ";
 
 
 void print_welcome_message(){
-    char input[1024];
+    char input[MAX_INPUT_SIZE];
     write(STDOUT_FILENO, welcome_message, strlen(welcome_message));
     write(STDOUT_FILENO, prompt_message, strlen(prompt_message));
     read(STDIN_FILENO,input,strlen(input));
